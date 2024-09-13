6b897d4420fe003ec2bfa2bfb70e13d252ebee58
arihant-jain0 <jainari1208@gmail.com>
2024-09-13 23:08:21 +0530

    test :docker file & ci/cd workflow

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,7 +5,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-     welcome to Aicodex
+     welcome to Aicodex by arihant jain
     </div>
   );
 }
